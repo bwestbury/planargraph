9429a5d8a1e8e8993c60d586949304aad0cdcf1c
David Howden <dhowden@gmail.com>
2008-11-25 10:29:58 +0000

    Removed executables and graph files.  Also moved testing code to tests directory.

--- tests/test.cpp
+++ tests/test.cpp
@@ -7,28 +7,27 @@
 
 #include "../PlanarGraph.hpp"
 
-#include <iostream>
+// #include <iostream>
 
-using namespace std;
+// using namespace std;
 
 /*
    Testing configuration, indexing and circle packing on premade polygons glued together.
 */
 void test()
 {
-	cout << "Boo!";
-	Graph g;
+	Graph g = Graph::createPolygon(64);
 	// g = Graph::createPolygon(3);
 	// Graph g1 = Graph::createPolygon(4);
 	// Graph g2 = Graph::createPolygon(5);
 	// Graph g3 = Graph::glue(g, g1, 1);
 	// Graph g4 = Graph::glue(g3, g2, 1);
 	// Graph g5 = Graph::glue(g4, g, 2);
-	g = Graph::createPolygon(4);
+	// g = Graph::createPolygon(4);
 
-	int boundary[4] = {1,1,1,1};
+	int boundary[64] = {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
 
-	drawGraph(g, boundary, "drawing_test.svg", false);
+	drawGraph(g, boundary, "drawing.eps", true);
 }
 
 int main()
